f5488d90873233c9940ed26ebe3698aaedb9917a
Patrick Alexandre <palexandre822@gmail.com>
2024-03-13 17:16:20 -0600

    revised project name in files

--- asl-tutor/src/app/app.component.spec.ts
+++ asl-tutor/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'als-tutor' title`, () => {
+  it(`should have the 'asl-tutor' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('als-tutor');
+    expect(app.title).toEqual('asl-tutor');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, als-tutor');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, asl-tutor');
   });
 });
