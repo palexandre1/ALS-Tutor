8d3e6c940368c87a9951a5737dd5573a7cf2fbbf
Patrick Alexandre <palexandre822@gmail.com>
2024-03-14 00:34:07 -0600

    integrated webcam functionality

--- asl-tutor/src/app/app.component.ts
+++ asl-tutor/src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Injectable } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WebcamImage } from 'ngx-webcam';
+import { CameraComponent } from './camera/camera.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, CameraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'ASL-Tutor';
+  public title: string;
+  public visible: boolean;
+  public webcamImage: WebcamImage | null;
+
+  constructor() {
+    this.title = 'ASL-Tutor';
+    this.visible = false;
+    this.webcamImage = null;
+  }
+
+  toggleCamera() {
+    this.visible = !this.visible;
+  }
+
+  handleImage(webcamImage: WebcamImage) {
+    this.webcamImage = webcamImage;
+  }
 }
