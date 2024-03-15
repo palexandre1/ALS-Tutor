ead7bf36815378a4593df6fe0fdb4f5e49b3d689
Patrick Alexandre <palexandre822@gmail.com>
2024-03-15 12:52:05 -0600

    tensorflow docker setup for mac M2 chip

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,15 @@
 from flask import Flask,jsonify,request
 import numpy as np
-# import os
-# import tensorflow as tf
+import os
+import tensorflow as tf
 # import tensorflow_datasets as tfds
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return 'Hello Flask!!'
+    print(tf.__version__)
+    return 'Hello and welcome to Docker!!'
 
 if __name__=="__main__":
     app.run(debug=True,host="0.0.0.0",port=8080)
