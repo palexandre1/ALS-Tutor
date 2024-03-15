7492a3f349cd177343d51a3448557fdeab571f2e
Patrick Alexandre <palexandre822@gmail.com>
2024-03-14 22:27:13 -0600

    installed tensorflow and other dependencies

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask,jsonify,request
+import numpy as np
+# import os
+# import tensorflow as tf
+# import tensorflow_datasets as tfds
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return 'Hello to Flask!'
+    return 'Hello Flask!!'
 
 if __name__=="__main__":
     app.run(debug=True,host="0.0.0.0",port=8080)
