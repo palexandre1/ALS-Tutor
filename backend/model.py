6f944b0de1b68ef756254e7a10a1aec167fff1e8
Patrick Alexandre <palexandre822@gmail.com>
2024-03-18 21:36:21 -0600

    gitignore fixed

--- backend/model.py
+++ backend/model.py
@@ -9,6 +9,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
+# Load dataset from directory
 train_ds = tf.keras.utils.image_dataset_from_directory(
   directory='asl_dataset/',
   validation_split=0.2,
@@ -30,24 +31,38 @@ print(class_names)
 
 AUTOTUNE = tf.data.AUTOTUNE
 
+# Optimize Datasets
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 validation_ds = validation_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 normalization_layer = layers.Rescaling(1./255)
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 
+# Define Variables
 num_classes = len(class_names)
 img_height = 256
 img_width = 256
 input_shape = (img_height, img_width, 3)
 
+
+# Data Augmentation
+data_augmentation = keras.Sequential([
+    layers.RandomFlip("horizontal"),
+    layers.RandomRotation(0.1),
+    layers.RandomZoom(0.1),
+  ])
+
+#Create Model
 model = Sequential([
-  layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=input_shape),
+  layers.Input(shape=input_shape),
+  data_augmentation,
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
+  layers.Dropout(0.2),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
@@ -59,7 +74,9 @@ model.compile(optimizer='adam',
 
 model.summary()
 
-epochs=10
+epochs=15
+
+# Train model
 history = model.fit(
   normalized_ds,
   validation_data=validation_ds,
@@ -68,44 +85,26 @@ history = model.fit(
 
 # Save the model
 model.save('asl_model.keras')
-# model_path = 'asl_model.keras'
-# export_path = "serving/"
-# new_model = tf.keras.models.load_model(model_path)
-# tf.saved_model.save(new_model, export_path)
 
-# acc = history.history['accuracy']
-# val_acc = history.history['val_accuracy']
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
 
-# loss = history.history['loss']
-# val_loss = history.history['val_loss']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
-# epochs_range = range(epochs)
+epochs_range = range(epochs)
 
-# plt.figure(figsize=(8, 8))
-# plt.subplot(1, 2, 1)
-# plt.plot(epochs_range, acc, label='Training Accuracy')
-# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-# plt.legend(loc='lower right')
-# plt.title('Training and Validation Accuracy')
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
 
-# plt.subplot(1, 2, 2)
-# plt.plot(epochs_range, loss, label='Training Loss')
-# plt.plot(epochs_range, val_loss, label='Validation Loss')
-# plt.legend(loc='upper right')
-# plt.title('Training and Validation Loss')
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
 
-# plt.savefig('analysis.png', bbox_inches='tight')
-
-# img = tf.keras.utils.load_img(
-#     "test.jpeg", target_size=(img_height, img_width)
-# )
-# img_array = tf.keras.utils.img_to_array(img)
-# img_array = tf.expand_dims(img_array, 0) # Create a batch
-
-# predictions = model.predict(img_array)
-# score = tf.nn.softmax(predictions[0])
-
-# print(
-#     "This image most likely belongs to {} with a {:.2f} percent confidence."
-#     .format(class_names[np.argmax(score)], 100 * np.max(score))
-# )
+plt.savefig('analysis.png', bbox_inches='tight')
